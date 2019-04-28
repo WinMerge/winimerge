04db9988c0d53b1af524563e8154cbae9dfe7cb3
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2019-04-28 11:32:43 +0900

    Bump revision to 1.0.9.0

--- SetVersion.cmd
+++ SetVersion.cmd
@@ -1,4 +1,4 @@
 set MAJOR=1
 set MINOR=0
-set REVISION=8
+set REVISION=9
 set PATCHLEVEL=0
--- Version.h
+++ Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        1,0,8,0
-#define PRODUCTVER     1,0,8,0
-#define STRFILEVER     "1.0.8.0"
-#define STRPRODUCTVER  "1.0.8.0"
+#define FILEVER        1,0,9,0
+#define PRODUCTVER     1,0,9,0
+#define STRFILEVER     "1.0.9.0"
+#define STRPRODUCTVER  "1.0.9.0"
