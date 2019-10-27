89f56ee179a7477d90813bc8e2f79abb01d59b71
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2019-10-27 15:58:09 +0900

    BuildArc.cmd: Use VS2017's vcomp140.dll

--- BuildArc.cmd
+++ BuildArc.cmd
@@ -4,6 +4,10 @@ call SetVersion.cmd
 set DISTDIR=.\Dist
 set path="%ProgramFiles%\7-zip";"%ProgramFiles(x86)%\7-zip";%path%
 
+for /f "usebackq tokens=*" %%i in (`"%programfiles(x86)%\microsoft visual studio\installer\vswhere.exe" -version [15.0^,16.0^) -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set InstallDir=%%i
+)
+
 mkdir "%DISTDIR%\WinIMerge" 2> NUL
 mkdir "%DISTDIR%\WinIMerge\bin" 2> NUL
 mkdir "%DISTDIR%\WinIMerge\bin64" 2> NUL
@@ -14,8 +18,8 @@ copy Build\Release\cidiff.exe "%DISTDIR%\WinIMerge\bin"
 copy Build\x64\Release\WinIMerge.exe "%DISTDIR%\WinIMerge\bin64"
 copy Build\x64\Release\WinIMergeLib.dll "%DISTDIR%\WinIMerge\bin64"
 copy Build\x64\Release\cidiff.exe "%DISTDIR%\WinIMerge\bin64"
-copy "%VS140COMNTOOLS%\..\..\VC\redist\x86\Microsoft.VC140.OpenMP\vcomp140.dll" "%DISTDIR%\WinIMerge\bin"
-copy "%VS140COMNTOOLS%\..\..\VC\redist\x64\Microsoft.VC140.OpenMP\vcomp140.dll" "%DISTDIR%\WinIMerge\bin64"
+copy "%InstallDir%\VC\Redist\MSVC\14.16.27012\x86\Microsoft.VC141.OpenMP\vcomp140.dll" "%DISTDIR%\WinIMerge\bin"
+copy "%InstallDir%\VC\Redist\MSVC\14.16.27012\x64\Microsoft.VC141.OpenMP\vcomp140.dll" "%DISTDIR%\WinIMerge\bin64"
 
 copy GPL.txt "%DISTDIR%\WinIMerge"
 copy freeimage-license-gplv2.txt "%DISTDIR%\WinIMerge"
