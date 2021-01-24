3246ad49617b2ab06e8b372c2c63065f8c02ceb1
datadiode <datadiode@users.noreply.github.com>
2021-01-25 00:05:22 +0100

    Generate version info through https://github.com/Thell/git-vs-versioninfo-gen (#16)

--- Version.h
+++ Version.h
@@ -1,4 +1,6 @@
-#define FILEVER        1,0,24,0
-#define PRODUCTVER     1,0,24,0
-#define STRFILEVER     "1.0.24.0"
-#define STRPRODUCTVER  "1.0.24.0"
+#include "gen-versioninfo.h"
+
+#define FILEVER        GEN_VER_DIGITAL_VERSION
+#define PRODUCTVER     GEN_VER_DIGITAL_VERSION
+#define STRFILEVER     GEN_VER_VERSION_STRING
+#define STRPRODUCTVER  GEN_VER_VERSION_STRING
