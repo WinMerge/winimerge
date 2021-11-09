5527076459c3b5593aaa5f8b0c56f6666485544e
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2021-11-09 22:49:01 +0900

    Added VS2022 support

---
+++ BuildBin.vs2022.cmd
@@ -0,0 +1,40 @@
+cd /d "%~dp0"
+
+del /s Build\*.exe
+del /s BuildTmp\*.res
+del gen-versioninfo.h
+
+setlocal
+for /f "usebackq tokens=*" %%i in (`"%programfiles(x86)%\microsoft visual studio\installer\vswhere.exe" -version [17.0^,18.0^) -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set InstallDir=%%i
+)
+if exist "%InstallDir%\Common7\Tools\vsdevcmd.bat" (
+  call "%InstallDir%\Common7\Tools\vsdevcmd.bat
+)
+
+for %%i in ( ^
+  ..\freeimage\Source\FreeImageLib\FreeImageLib.vcxproj ^
+  ..\freeimage\Wrapper\FreeImagePlus\FreeImagePlus.vcxproj ^
+  src\WinIMergeLib.vcxproj ^
+  src\WinIMerge.vcxproj ^
+  src\cidiff.vcxproj ^
+  ) do (
+  MSBuild %%i /t:build /p:Configuration=Release /p:Platform="Win32" /p:PlatformToolset=v142 || goto :eof
+  MSBuild %%i /t:build /p:Configuration=Release /p:Platform="x64" /p:PlatformToolset=v142 || goto :eof
+  MSBuild %%i /t:build /p:Configuration=Release /p:Platform="ARM64" /p:PlatformToolset=v142 || goto :eof
+)
+
+if exist "%SIGNBAT_PATH%" (
+  "%SIGNBAT_PATH%" Build\Release\WinIMerge.exe
+  "%SIGNBAT_PATH%" Build\Release\WinIMergeLib.dll
+  "%SIGNBAT_PATH%" Build\Release\cidiff.exe
+  "%SIGNBAT_PATH%" Build\x64\Release\WinIMerge.exe
+  "%SIGNBAT_PATH%" Build\x64\Release\WinIMergeLib.dll
+  "%SIGNBAT_PATH%" Build\x64\Release\cidiff.exe
+  "%SIGNBAT_PATH%" Build\ARM64\Release\WinIMerge.exe
+  "%SIGNBAT_PATH%" Build\ARM64\Release\WinIMergeLib.dll
+  "%SIGNBAT_PATH%" Build\ARM64\Release\cidiff.exe
+)
+
+endlocal
+
